c789b2e64d28906d6434126ddc98c5d2d68d54be
ReyLpol <lazarevdima8671@gmail.com>
2022-03-27 16:27:00 +0700

    commit 2 on s1

--- app/src/main/java/com/example/projecgitlazarev/MainActivity.java
+++ app/src/main/java/com/example/projecgitlazarev/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+//commit 2 on s1
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
